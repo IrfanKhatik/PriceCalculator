40eec2dc5fe1fcc77fa5c535616a7bfed1fa1f0a
Irfan Khatik <iKhatik@blackberry.com>
2018-11-20 13:02:50 +0530

    Initial Commit

---
+++ DiscountsDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiscountsDemo
+//
+//  Created by Irfan Khatik on 20/11/18.
+//  Copyright © 2018 Discounts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
